5728dfb0c14b2776084362a689f20d3887ea798a
ingussuveiks-dev <ingussuveiks@gmail.com>
2026-06-16 10:32:27 +0300

    lai uz custom arī visi ceļi strādātu

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -22,3 +22,91 @@
 #      "" CACHE PATH "Default install path")
 #  endif()
 #endif()
+
+function(set_local_cache_path variable value type description)
+  if(EXISTS "${value}" AND (NOT DEFINED ${variable} OR "${${variable}}" STREQUAL "" OR "${${variable}}" MATCHES "-NOTFOUND$"))
+    set(${variable} "${value}" CACHE ${type} "${description}" FORCE)
+  endif()
+endfunction()
+
+if(CMAKE_HOST_WIN32)
+  set_local_cache_path(BOOST_ROOT
+    "C:/local/boost_1_85_0"
+    PATH
+    "Boost root directory")
+
+  set_local_cache_path(Boost_INCLUDE_DIR
+    "C:/local/boost_1_85_0"
+    PATH
+    "Boost include directory")
+
+  set_local_cache_path(BOOST_LIBRARYDIR
+    "C:/local/boost_1_85_0/lib64-msvc-14.3"
+    PATH
+    "Boost library directory")
+
+  set_local_cache_path(MYSQL_INCLUDE_DIR
+    "C:/wamp64/bin/mysql/mysql5.7.44/include"
+    PATH
+    "Specify the directory containing mysql.h.")
+
+  set_local_cache_path(MYSQL_LIBRARY
+    "C:/wamp64/bin/mysql/mysql5.7.44/lib/libmysql.lib"
+    FILEPATH
+    "Specify the location of the mysql library here.")
+
+  set_local_cache_path(MYSQL_EXECUTABLE
+    "C:/wamp64/bin/mysql/mysql5.7.44/bin/mysql.exe"
+    FILEPATH
+    "path to your mysql binary.")
+
+  set_local_cache_path(OPENSSL_ROOT_DIR
+    "C:/Program Files/OpenSSL-Win64"
+    PATH
+    "OpenSSL root directory")
+
+  set_local_cache_path(OPENSSL_INCLUDE_DIR
+    "C:/Program Files/OpenSSL-Win64/include"
+    PATH
+    "OpenSSL include directory")
+
+  set_local_cache_path(LIB_EAY_LIBRARY_RELEASE
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libcrypto.lib"
+    FILEPATH
+    "OpenSSL crypto release library")
+
+  set_local_cache_path(LIB_EAY_RELEASE
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libcrypto.lib"
+    FILEPATH
+    "OpenSSL crypto release library")
+
+  set_local_cache_path(SSL_EAY_LIBRARY_RELEASE
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libssl.lib"
+    FILEPATH
+    "OpenSSL SSL release library")
+
+  set_local_cache_path(SSL_EAY_RELEASE
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libssl.lib"
+    FILEPATH
+    "OpenSSL SSL release library")
+
+  set_local_cache_path(LIB_EAY_LIBRARY_DEBUG
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MDd/libcrypto.lib"
+    FILEPATH
+    "OpenSSL crypto debug library")
+
+  set_local_cache_path(LIB_EAY_DEBUG
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MDd/libcrypto.lib"
+    FILEPATH
+    "OpenSSL crypto debug library")
+
+  set_local_cache_path(SSL_EAY_LIBRARY_DEBUG
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MDd/libssl.lib"
+    FILEPATH
+    "OpenSSL SSL debug library")
+
+  set_local_cache_path(SSL_EAY_DEBUG
+    "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MDd/libssl.lib"
+    FILEPATH
+    "OpenSSL SSL debug library")
+endif()
